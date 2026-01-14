9efbaad045f0363fa49d45177f3b7b7eea935d50
Ali Farahani <afarahan@nu.edu>
2026-01-14 14:55:19 -0800

    removed lint/flask 8

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
 import numpy as np
-import pickle
+# import pickle
 import joblib
 app = Flask(__name__)
 filename = 'file_iris1.pkl'
-#model = pickle.load(open(filename, 'rb'))
+# model = pickle.load(open(filename, 'rb'))
 model = joblib.load(filename)
-#model = joblib.load(filename)
+# model = joblib.load(filename)
 @app.route('/')
 def index(): 
     return render_template('index.html')
